6598b42551aedf4de78e618490feff2d77024db6
Henrique Lino Pacheco <lino4000@protonmail.com>
2021-09-19 23:19:50 -0300

    Starting Spring project

---
+++ src/main/java/com/lino4000/petFinder/PetFinderApplication.java
@@ -0,0 +1,13 @@
+package com.lino4000.petFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lino4000/petFinder/PetFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lino4000.petFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
